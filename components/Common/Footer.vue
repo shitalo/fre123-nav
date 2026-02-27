f4968c80e811cae8d04e2c8e7519a6fc6afc4466
是她咯 <915007735@qq.com>
2026-02-28 00:23:14 +0800

    fix: footer

--- components/Common/Footer.vue
+++ components/Common/Footer.vue
@@ -2,17 +2,17 @@
 	<footer class="text-left text-[#6c757d] p-6 bg-[rgba(248,249,250,1)] h-[70px]">
 		<section class="flex justify-center">
 			<!-- Left -->
-			<!--<div class="text-[14px] h-[30px]">
+			<div class="text-[14px] h-[30px]">
 				<span
 					>Powered by
 					<a
-						href="https://www.fre123.com"
+						href=""
 						target="_blank"
 						class="decoration-transparent text-[#a4715c]"
 						>FRE123</a
 					>
 				</span>
-			</div>-->
+			</div>
 			<!-- Left -->
 
 			<div v-if="footerConfig.right.is_show" class="flex-grow"></div>
